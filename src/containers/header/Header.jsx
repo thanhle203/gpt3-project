a603021b2e0298804f302ae0bb216902fb0cd0b5
Thanh Le <thanhwaslike@gmail.com>
2023-06-06 22:55:13 -0700

    Completed Header portion

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
     <div className='gpt3__header section__padding' id='home'>
       <div className='gpt3__header-content'>
         <h1 className='gradient__text'>Let's Build Something amazing with GPT-3 OpenAI</h1>
+        <p>OpenAI is an artificial intelligence (AI) research organization that aims to develop and promote friendly and beneficial AI systems. It was founded in December 2015 and has gained prominence for its advancements in natural language processing and machine learning.</p>
+
+        <div className='gpt3__header-content__input'>
+          <input type='email' placeholder='Your Email Address'></input>
+          <button type='button'>Get Started</button>
+        </div>
+
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt='people' />
+          <p> Over 1,600 people requested access in the last 24 hours</p>
+        </div>
       </div>
+
+      <div className='gpt3__header-image'>
+        <img src={ai} alt='ai' />
+      </div>
+
     </div>
   )
 }
